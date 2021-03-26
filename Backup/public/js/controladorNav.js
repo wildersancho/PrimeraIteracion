04f95d25dd39c7ceb88d0c05e4537e78188d42f5
keko-cmd <mjmccrorypsn@hotmail.ca>
2021-03-25 20:36:17 -0600

    Remove de backup

--- Backup/public/js/controladorNav.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const cambiar_estilo_navegacion = () => {
-    var x = document.getElementById("header-principal");
-    if (x.className === "header-principal") {
-        x.className += " responsive";
-    } else {
-        x.className = "header-principal";
-    }
-}
